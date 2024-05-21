6029d2f8a90b0a86c3881605a7237389de028b42
ashwinnath06 <150225977+ashwinnath06@users.noreply.github.com>
2024-05-21 14:51:00 +0530

    Add files via upload

---
+++ Ashwin JavaScript/script.js
@@ -0,0 +1,93 @@
+//let a = 10;
+//let b=20;
+
+//console.log(a+b);
+//console.log(a-b);
+//console.log(a*b);
+//console.log(a/b);
+//console.log(a%b);
+
+
+//postfix increment
+//let a= 1;
+//let b= a++;
+//console.log(a);
+//console.log(b);
+
+//prefix increment
+//let c= 1;
+//let d= ++c;
+//console.log(c);
+//console.log(d);
+
+//let car={
+// model:"SUPERCAR",
+//  color:"White"
+    
+//};
+
+//console.log(car);
+
+//let persons={
+//     fname:"Ashwin Nath K R",
+//   //  address:{
+//         street:"abc",
+//         place:"xyz",
+//         pin:123
+//     },
+//     age:10
+// };
+// //console.log(persons.address);
+//console.log(persons.fname);
+//console.log(persons.age);
+
+// var toothbrush=new Object();
+// toothbrush.brand="Close Up";
+// toothbrush.color="Red";
+
+// console.log(toothbrush);
+
+
+//Concatination
+    // let fname="Ashwin";
+    // let lname="Nath";
+    // console.log(`${fname}${lname}`);
+    // let m =5;
+    // let n =6;
+    // console.log(`the sum is ${m+n}`);
+
+    //Array
+    // let fruits=["Apple","Mango","Orange",23]
+    // console.log(fruits);
+    // fruits.push("Grapes");
+    // console.log(fruits);
+    // console.log(fruits.length);
+    // fruits[2]="Pineapple";
+    // console.log(fruits);
+    // fruits.unshift("Pear");
+    // console.log(fruits);
+    // fruits.pop();
+    // console.log(fruits);
+    // fruits.shift();
+    // console.log(fruits);
+
+    //Array of object
+    let bsc=[
+        {
+            fname:"Ashwin",
+            age:18,
+        },
+        {
+            fname:"Achu",
+            age:6,
+        },
+        {
+            fname:"SPY",
+            age:16,
+        },
+    ];
+    console.log(bsc[2].age);
+        
+        
+        
+    
